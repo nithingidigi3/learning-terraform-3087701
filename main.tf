a46f6f3c886f9831dfb9a5f12c12648ce2ba17bb
Nithin Gidigi <121715127+nithingidigi3@users.noreply.github.com>
2023-01-19 15:47:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,14 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-
- module "blog_vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -30,24 +29,29 @@ data "aws_ami" "app_ami" {
     Terraform = "true"
     Environment = "dev"
   }
-} 
-   
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
-  
+
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
-  name = "alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
@@ -57,16 +61,10 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "aws_instance.blog.id"
-          port = 80
-        }
-      }
     }
   ]
 
@@ -81,17 +79,16 @@ module "blog_alb" {
   tags = {
     Environment = "dev"
   }
-}  
-    
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
-  name = "modseg"
-  vpc_id = module.blog_vpc.vpc_id
-  
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules = ["all-all"] 
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
