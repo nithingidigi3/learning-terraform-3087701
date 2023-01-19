887fff654b0d9fa10f9dd6180379d7ab36b506ea
Nithin Gidigi <121715127+nithingidigi3@users.noreply.github.com>
2023-01-19 13:26:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,13 +11,12 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["self"] # Bitnami
+  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   tags = {
     Name = "HelloWorld"
   }
