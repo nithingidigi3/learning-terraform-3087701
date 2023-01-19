8404b79bf04fad3218e67698d370e06347ee1215
Nithin Gidigi <121715127+nithingidigi3@users.noreply.github.com>
2023-01-19 14:54:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -27,12 +23,29 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
+  
+ module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+} 
+  
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   name = "modseg"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
   
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
